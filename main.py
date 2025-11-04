250e61f2f06f1d3c877716c1aa6a8b86e58b84fb
OliveiraEGL <joaopedrooliveiraaguiar090@gmail.com>
2025-11-03 23:59:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import flask
+from flask import Flask, render_template
+from apimercadopago import create_payment
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    link_iniciar_pagamento = create_payment()
+    return render_template('index.html', link_pagamento=link_iniciar_pagamento)
+
+@app.route('/compracerta')
+def about_page():
+    return render_template('download.html')
+@app.route('/compraerrada')
+def contact_page():
+    return render_template('compraerrada.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
